b9b695415da4545d40dae18a529a8a6286c86ae4
BohanWu <819186192@qq.com>
2023-07-17 21:08:31 +0800

    test: query not support

--- pkg/tsdb/index/tag_pairs_generator/full_permutation_gen_test.go
+++ pkg/tsdb/index/tag_pairs_generator/full_permutation_gen_test.go
@@ -16,11 +16,11 @@ var (
 
 var g FullPermutationGen
 
-func TestGenerateQuery(t *testing.T) {
-	queries := g.GenerateQueryTagsSlice(tagKeyNum, tagValueNum)
-	assert.Equal(t, tsi2.PowUint64((tagValueNum+1), tagKeyNum)-1, uint64(len(queries)))
-	// fmt.Printf("%v\n", queries)
-}
+// func TestGenerateQuery(t *testing.T) {
+// 	queries := g.GenerateQueryTagsSlice(tagKeyNum, tagValueNum)
+// 	assert.Equal(t, tsi2.PowUint64((tagValueNum+1), tagKeyNum)-1, uint64(len(queries)))
+// 	// fmt.Printf("%v\n", queries)
+// }
 
 func TestGenerateInserts(t *testing.T) {
 	inserts := g.GenerateInsertTagsSlice(tagKeyNum, tagValueNum)
